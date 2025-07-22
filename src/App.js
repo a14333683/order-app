0bfcd07f0213d130c84cc5118fe1ec74ead55608
SWing <a14333683@gmail.com>
2025-07-23 01:36:46 +0800

    Commit v2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Login from './pages/Login'
+import UserRegister from './pages/UserRegister';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -10,12 +11,10 @@ import Header from './components/Header';
 export default function App() {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-      </div>
-           
       <Routes>
+        <Route path="/" element={<Header />} />
         <Route path="/Login" element={<Login />} />
+        <Route path="/Register" element={<UserRegister />} />
       </Routes>
     </BrowserRouter>
   );
